d4b4632628408976d7030313b329bd8249d7d9fa
VasylynaSidei <vasilisasidey13@gmail.com>
2024-01-25 12:06:57 +0100

    add working useContext

--- src/components/GeneralQuestion/notes.jsx
+++ src/components/GeneralQuestion/notes.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./GeneralQuestion.css";
 import { useParams } from "react-router-dom";
-import { useAnswerContext } from "../context/AnswerContext";
 
 function GeneralQuestion() {
-  const obj = useAnswerContext();
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [quizComplete, setQuizComplete] = useState(false);
   let { category } = useParams();
 
   const fetchingData = async () => {
@@ -29,7 +32,7 @@ function GeneralQuestion() {
       const response = await fetch(url);
       const result = await response.json();
       console.log(result);
-      obj.setQuestions(
+      setQuestions(
         result.map((item) => {
           return {
             incorrect_answers: item.incorrectAnswers,
@@ -42,6 +45,7 @@ function GeneralQuestion() {
       console.error(error.message);
     }
   };
+
   const decodeHtmlEntities = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent;
@@ -54,36 +58,48 @@ function GeneralQuestion() {
     ];
     return shuffledAnswers.sort(() => Math.random() - 0.5);
   };
+
+  const handleAnswerClick = (answer) => {
+    setSelectedAnswer(answer);
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setSelectedAnswer(null);
+
+    if (answer === questions[currentQuestionIndex].correct_answer) {
+      setCorrectAnswers((prevCount) => prevCount + 1);
+    }
+  };
+
   useEffect(() => {
     fetchingData();
   }, []);
 
-  if (obj.questions.length === 0) {
+  if (questions.length === 0) {
     return <div>Loading...</div>;
   }
 
-  const isQuizComplete = obj.currentQuestionIndex === obj.questions.length - 1;
+  const isQuizComplete = currentQuestionIndex === questions.length - 1;
 
-  const currentQuestion = obj.questions[obj.currentQuestionIndex];
+  const currentQuestion = questions[currentQuestionIndex];
   const shuffledAnswers = shuffleAnswers(currentQuestion);
+
   return (
     <>
       {isQuizComplete ? (
         <div>
           <h4>Quiz Complete!</h4>
-          <p>Correct Answers: {obj.correctAnswers}</p>
+          <p>Correct Answers: {correctAnswers}</p>
           <button>Back to menu</button>
           <button>Play again</button>
         </div>
       ) : (
-        <div>
-          <h3>{decodeHtmlEntities(currentQuestion.question)}</h3>
+        <div className="questionanswer">
+          <div className="question">
+            {decodeHtmlEntities(currentQuestion.question)}
+          </div>
           <ul>
             {shuffledAnswers.map((answer, index) => (
-              <li key={index}>
-                <button onClick={() => obj.handleAnswerClick(answer)}>
-                  {decodeHtmlEntities(answer)}
-                </button>
+              <li key={index} onClick={() => handleAnswerClick(answer)}>
+                {decodeHtmlEntities(answer)}
               </li>
             ))}
           </ul>
