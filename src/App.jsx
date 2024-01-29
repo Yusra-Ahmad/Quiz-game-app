c7e3e30dfb4e8e9ff4f521dcd07be6c56a5d10bb
sadhana singh <sadhana.singh2092@gmail.com>
2024-01-29 10:42:00 +0100

    added some finishing changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import { HomePage } from "./components/Main/HomPage";
 import { QuizSelection } from "./components/QuizSelection/QuizSelection";
 import { Route, Routes } from "react-router-dom";
 
-
 function App() {
   return (
     <>
-  
-
       <Routes>
         <Route index element={<HomePage />}></Route>
         <Route path="quiz-selection" element={<QuizSelection />}></Route>
-        <Route path="quiz/:category" element={< GeneralQuestion/>}></Route>
+        <Route path="quiz/:category" element={<GeneralQuestion />}></Route>
       </Routes>
-
     </>
   );
 }
